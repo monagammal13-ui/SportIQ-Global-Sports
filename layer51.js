e102d8b7713a2c841e6462a0af67cdbe82b1d1c5
monagammal13-ui <monagammal13@gmail.com>
2025-12-29 20:12:10 -0700

    Add files via upload
    Batch 2 - API JSON File upload

---
+++ layer51.js
@@ -0,0 +1,11 @@
+
+{
+    "id": "layer-051",
+        "name": "Global Trending Topics Dashboard",
+            "version": "1.0.0",
+                "refreshInterval": 30000,
+                    "sources": ["internal-analytics", "twitter-api", "news-rss"],
+                        "displayLimit": 10,
+                            "categories": ["football", "basketball", "tennis", "esports"],
+                                "alertThreshold": 1000000
+}
